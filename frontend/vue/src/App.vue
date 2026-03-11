abf077c9bf2a26fe7162613236e4f1b2cdf6a4ed
Reisentyan <16430339+reisentyan@user.noreply.gitee.com>
2026-03-11 14:36:36 +0800

    refactor:重构界面代码，新增开场动画，增加功能梳理文件

--- frontend/vue/src/App.vue
+++ frontend/vue/src/App.vue
@@ -1,14 +1,22 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import HeadPage from './components/HeadPage.vue';
 import BodyPage from './components/BodyPage.vue';
+import Curtain from './components/Raise-the-curtain.vue';
+
+// 控制开场动画的开关，默认开启
+const showIntro = ref(true);
 </script>
 
 <template>
+	<Transition name="fade">
+		<Curtain v-if="showIntro" @close="showIntro = false" />
+	</Transition>
+
 	<div class="layout">
 		<div class="container"><HeadPage /></div>
 		<BodyPage />
 	</div>
-	<a href="./src/temp/开发须知.md" download>点击下载开发须知</a>
 </template>
 
 <style scoped>
